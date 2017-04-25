ed12e1e466855afdf21b855b959a69ee4c392999
Mitch Cravens <mcravens@digitalairstrike.com>
2017-04-24 17:16:20 -0700

    converting to react-md.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './App.css';
 
-const App = () => (
-  <div className={styles.app}>
-    <h2>Hello, Mike!</h2>
-  </div>
-);
+import {load} from 'webfontloader'
+// https://github.com/typekit/webfontloader
+import Button from 'react-md/lib/Buttons/Button'; // TODO this isn't working
+
+const App = () => {
+  load({
+    google: {
+      families: ['Roboto:300,400,500,700,400italic', 'Material Icons']
+    }
+  })
+  return (
+    <div className={styles.app}>
+      <h2>Hello, Mike!</h2>
+      <Button raised primary label="Spock" iconClassName="fa fa-hand-spock-o" />
+    </div>
+  )
+}
 
 export default App;
